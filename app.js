52f9f43ba32e61aa16472a9da5996d6f5512e1d4
grantb4812 <grantb4812@gmail.com>
2015-10-20 16:21:58 -0500

    working on the increment and decrement times

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-  $('.increase-button').on('click', function(){
+  $('.break-increase-button').on('click', function(){
     increase();
+  });
+  $('.break-decrease-button').on('click', function(){
+    decrease();
   });
 });
 
-var increase = function () {
-  var num = 0;
-  num =+ 1;
-  $('.break-length').replaceWith(num);
+var breakIncreaseNum = 0;
+console.log(breakIncreaseNum);
+var increase = function() {
+  breakIncreaseNum += 1;
+  $('.break-length').replaceWith('<span class="break-length">'+breakIncreaseNum+'</span>')
+  return breakIncreaseNum;
 };
+
+console.log(breakIncreaseNum);
+var breakDecreaseNum = breakIncreaseNum;
+var decrease = function() {
+  breakDecreaseNum -= 1;
+  $('.break-length').replaceWith('<span class="break-length">'+breakDecreaseNum+'</span>')
+  console.log(breakDecreaseNum);
+}; 
